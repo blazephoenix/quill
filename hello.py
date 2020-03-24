d9f4eeabbdead6e9f380b6c2ce86223ace4dcb58
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2020-03-24 17:11:43 +0530

    modified gitignore

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
+@app.route("/api")
 def home():
     return "Hello World"
 
